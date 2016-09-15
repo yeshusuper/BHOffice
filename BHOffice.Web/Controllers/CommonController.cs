1c4982302e1a26737657045faee1d8a3a3822d59
lam <yeshusuper@qq.com>
2016-09-15 12:49:01 +0800

    统一列表和表单样式

---
+++ BHOffice.Web/Controllers/CommonController.cs
@@ -0,0 +1,32 @@
+﻿using BHOffice.Core.Business;
+using BHOffice.Web.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHOffice.Web.Controllers
+{
+    public class CommonController : BaseController
+    {
+        private readonly IUserManager _UserManager;
+
+        public CommonController(IUserManager userManager)
+        {
+            _UserManager = userManager;
+        }
+
+        [ChildActionOnly]
+        public ActionResult LeftMenu()
+        {
+            var model = new Models.Common.LeftMenuModel();
+            if (this.CurrentUser != null)
+            {
+                model.IsAdmin = _UserManager.GetUser(this.CurrentUser.Uid).Role >= UserRoles.Admin;
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ BHOffice.Web/Models/Common/LeftMenuModel.cs
@@ -0,0 +1,13 @@
+﻿using BHOffice.Core.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BHOffice.Web.Models.Common
+{
+    public class LeftMenuModel
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
